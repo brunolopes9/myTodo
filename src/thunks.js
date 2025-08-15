4b6b304d5aa006024df289c198ebf908fb70724e
Bruno Lopes <bruno-lopes9@hotmail.com>
2025-08-15 20:21:27 +0100

    add thunk js function

--- src/thunks.js
+++ src/thunks.js
@@ -1,6 +1,18 @@
 import axios from 'axios';
+import {
+  loadingStarted,
+  loadingCompleted,
+  loadingFailed,
+} from '.loadingSlice';
 
 export const loadTodos = () => async (dispatch) => {
+  dispatch(loadingStarted());
+  try {
+ 
   const response = await axios.get('/api/todos');
   cons todos = response.data;
+  dispatch(loadingCompleted());
+} catch(e)  {
+  loadingFailed(e);
+}
 }
